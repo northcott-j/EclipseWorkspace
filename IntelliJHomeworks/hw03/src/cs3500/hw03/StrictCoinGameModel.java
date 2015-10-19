782e9ce0b1ea3c126e6d64449f7e43900824a006
Jonathan Northcott <northcott.j@chisigma.co>
2015-10-19 12:24:43 -0400

    Initial hw03 push

---
+++ IntelliJHomeworks/hw03/src/cs3500/hw03/StrictCoinGameModel.java
@@ -0,0 +1,37 @@
+package cs3500.hw03;
+
+/**
+ * You don't need to implement this class or any concrete subclasses
+ * for pset03.
+ */
+public final class StrictCoinGameModel implements CoinGameModel {
+  // (Exercise 2) Declare the fields needed to support the methods in
+  // the interface you’ve designed:
+
+  // (Exercise 3) Describe, as precisely as you can, your
+  // representation’s class invariants:
+
+  // (Exercise 4) Describe your constructor API here by filling in
+  // whatever arguments you need and writing good Javadoc. (You may
+  // declare any combination of constructors and static factory
+  // methods that you like, but you need not get fancy.)
+  /**
+   * Constructs [fill in comprehensive and clear Javadoc here]
+   *
+   * @param [you decide]
+   * @param [as many parameters as you like]
+   * @throws [same deal for exceptions]
+   */
+  protected StrictCoinGameModel(/* add the arguments here */) {
+    // You don't need to implement this constructor.
+    throw new UnsupportedOperationException("no need to implement this");
+  }
+
+  // You don't need to implement any methods or constructors. However,
+  // if you want to make sure your code compiles, you could have your
+  // IDE generate stubs for all the missing methods. This would also
+  // allow you to make sure that your tests in StrictCoinGameModelTest
+  // actually type check and compile against this class (though you
+  // don’t need to make them pass, because you don’t need to implement
+  // StrictCoinGameModel’s methods).
+}
